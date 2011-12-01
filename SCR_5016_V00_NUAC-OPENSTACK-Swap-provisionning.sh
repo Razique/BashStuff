9ca7cae54284f093ae3e512fec625f583aa39147
razique <razique.mahroua@gmail.com>
2011-12-01 19:33:20 +0100

    Add a condition which checks the total amount of memory and set the size accordingly
    Add a condition which checks the total amount of memory and set the
    size accordingly
    Add a function which unmounts the swap

--- SCR_5016_V00_NUAC-OPENSTACK-Swap-provisionning.sh
+++ SCR_5016_V00_NUAC-OPENSTACK-Swap-provisionning.sh
@@ -3,10 +3,12 @@
 # Shell : Bash
 # Description : Instances Swap provisionning script
 # Author : razique.mahroua@gmail.com
-# Actual version : Version 00
+# Actual version : Version 01
 
 # 		Revision note    
 # V00 : Initial version
+# V01 :	Add a condition which checks the total amount of memory and set the size accordingly
+#		Add a function which unmounts the swap
 
 # This script creates a swapfile and enable it based on the instance's RAM. 
 # Because instance's flavor can be updated, the script should be run from /etc/rc.local
@@ -28,22 +30,40 @@
 # We retrieve the memory the server has
 TOTAL_MEMORY=`$AWK '/MemTotal/ {printf( "%.0f\n", $2/1024 )}' /proc/meminfo`
 
-# We calculate the swap size (Memory/2 + 2 mb)
-SWAP_SIZE=`echo $(($TOTAL_MEMORY/2+2))`
+# We calculate the swap size based on the available memory
+if [ $TOTAL_MEMORY -gt 512 ]; then 
+	SWAP_SIZE=`echo $(($TOTAL_MEMORY/2+2))`
+else
+	SWAP_SIZE=`echo $(($TOTAL_MEMORY*2+2))`
+fi
+
+# We setup a threshold
+if [ $SWAP_SIZE -gt 1024 ]; then
+	SWAP_SIZE=1024
+fi
 
 function create_swapfile() {
 	$DD if=/dev/zero of=$SWAP_FILE bs=1M count=$SWAP_SIZE
 	$MKSWAP $SWAP_FILE
 }
 
+function unmount_swap() {
+	$SWAPOFF $SWAP_FILE > /dev/null 2>&1
+}
+
 # If the file already exists, we don't bother to create another one
 if [ -f $SWAP_FILE ]; then
 	# We make sure the file is not busy first
-	$SWAPOFF $SWAP_FILE > /dev/null 2&>1
-	if [ $? -ne 0 ]; then
+	$LOGGER "Unmounting swap file..."
+	unmount_swap
+	OUTPUT=$?
+
+	if [ $OUTPUT -eq 0 ]; then
 		$LOGGER "Swapfile not mounted, nothing to do..."
+
 	else
-		$SWAPOFF $SWAP_FILE
+		$LOGGER "Unmounting swap file..."
+		unmount_swap
 	fi
 
 	ORIGINAL_FILE=`du -ac $SWAP_FILE | tail -1 | cut -f 1`
@@ -58,4 +78,4 @@ else
 fi
 
 # We enable the file
-$SWAPON $SWAP_FILE
+$SWAPON $SWAP_FILE 
